63c31e5fea1543000ad822ebdce90d8d985526f7
leszek3737 <32321506+leszek3737@users.noreply.github.com>
2017-10-26 13:04:37 +0200

    new version

---
+++ js/dane.js
@@ -0,0 +1,5 @@
+window.__koszDpsow__ = [4000, 3900, 3800, 3700, 3600, 3500, 3400];
+window.kryDochSamotnie = 1902;
+window.kryDochRodz = 1542;
+window.__kryDochSamotnie__ = 1902;
+window.__kryDochRodz__ = 1542;
---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+
+    document.addEventListener("DOMContentLoaded", function () {
+        fillSelectDefaulOptions(document.querySelector("#dps select"), window.__DPS__);
+        fillStep4();
+    });
+
+    function fillStep4() {
+        var addFamilyMemberBtn = document.querySelector(".addFamilyMemberBtn")
+        addFamilyMemberBtn.addEventListener("click", addFamilyMember);
+        
+    }
+
+    function addFamilyMember() {
+        var tr = document.createElement("tr");
+        tr.classList.add("czlonekRodziny");
+        tr.innerHTML = "<td><select></select></td>";
+        tr.innerHTML += "<td><input id='ileOs0' type='number'></td>";
+        tr.innerHTML += "<td><input id='przyG0' type='number'></td>";
+        tr.innerHTML += "<td><button class='removeFamilyMemberBtn'> tak</button></td>";
+        
+        var select = tr.querySelector("select");
+        fillSelectDefaulOptions(select, window.__stopiniePok__);
+        
+        
+        tr.querySelector(".removeFamilyMemberBtn")
+        
+        document.querySelector("#krok4 table").appendChild(tr);
+    }
+
+    function fillSelectDefaulOptions(container, list) {
+        list.forEach(function (obj) {
+            var option = document.createElement("option");
+            option.id = obj.id;
+            option.value = obj.id;
+            option.innerHTML = obj.name;
+            container.appendChild(option);
+        });
+    }
+})();
