75e8d955bd5d1a146df54223dd62cde2d4f60a67
leszek3737 <32321506+leszek3737@users.noreply.github.com>
2017-10-26 16:31:41 +0200

    and table to winik

--- js/main.js
+++ js/main.js
@@ -13,25 +13,36 @@
     }
 
     function addFamilyMember() {
+        // Krok 4
         var tr = document.createElement("tr");
-        var table = document.querySelector("#krok4 table");
         tr.classList.add("czlonekRodziny");
         tr.innerHTML = "<td><select></select></td>";
         tr.innerHTML += "<td><input id='ileOs0' type='number'></td>";
         tr.innerHTML += "<td><input id='przyG0' type='number'></td>";
         tr.innerHTML += "<td><button class='removeFamilyMemberBtn'> tak</button></td>";
-
         var select = tr.querySelector("select");
         fillSelectDefaulOptions(select, window.__stopiniePok__);
-
-
+        // wynik
+        var wynikTr = document.createElement("tr");
+        wynikTr.classList.add("WynikCzłonekRodziny");
+        wynikTr.innerHTML = "<td>-</td>";
+        wynikTr.innerHTML += "<td>-</td>";
+        wynikTr.innerHTML += "<td>-</td>";
+        wynikTr.innerHTML += "<td>-</td>";
+        //usuwanie
+        var table = document.querySelector("#krok4 table");
+        var wynikTabele = document.querySelector(".wynik table");
+        
         var removeFamilyMemberBtn = tr.querySelector(".removeFamilyMemberBtn");
         removeFamilyMemberBtn.addEventListener("click", function () {
-            table.removeChild(tr)
-        }, false)
+            table.removeChild(tr);
+            wynikTabele.removeChild(tr);
+        }, false) 
 
-
+        //dodawanie do  html
         document.querySelector("#krok4 table").appendChild(tr);
+        document.querySelector(".wynik table").appendChild(wynikTr);
+
     }
 
     function fillSelectDefaulOptions(container, list) {
